64564d6f498d98e6aa74ccfe630e460ed935fb23
KeerthanaM10 <keerthanam29112005@gmail.com>
2026-06-13 01:00:26 +0530

    Add files via upload

---
+++ Arrays/Guess Number Higher or Lower.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int guessNumber(int n) {
+        int s = 1;
+        int e = n;
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+
+            int res = guess(mid);
+
+            if (res == 0)
+                return mid;
+            else if (res == -1)
+                e = mid - 1;
+            else
+                s = mid + 1;
+        }
+
+        return -1;
+    }
+};
