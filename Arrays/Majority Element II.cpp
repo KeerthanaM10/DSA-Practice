154846a26baf5dc646f00bb262ef2f7fa545a3d0
KeerthanaM10 <keerthanam29112005@gmail.com>
2026-06-23 00:37:37 +0530

    Implement Majority Element II algorithm

---
+++ Arrays/Majority Element II.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<int> res;
+        int n = nums.size();
+        int count = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1]) {
+                count++;
+            } else {
+                if (count > n / 3) {
+                    res.push_back(nums[i - 1]);
+                }
+                count = 1;
+            }
+        }
+
+        
+        if (count > n / 3) {
+            res.push_back(nums[n - 1]);
+        }
+
+        return res;
+    }
+};
